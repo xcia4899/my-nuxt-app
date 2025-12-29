c669cc466173ba4da4603c44501dbc8ea06db2a7
xcia4899 <xcia48996531@gmail.com>
2025-12-29 16:08:11 +0800

    Header + Narbar

--- app.vue
+++ app.vue
@@ -1,17 +1,23 @@
-
-
 <template>
-<NuxtLink to="/" >首頁</NuxtLink>
-<NuxtLink to="/video" >影片頁</NuxtLink>
-
-  <div>
-    <NuxtPage/>
+  <Header></Header>
+  <Nabar></Nabar>
+  <div class="showView">
+    <NuxtPage />
   </div>
-
 </template>
 
-
 <script setup lang="ts">
-
-
+useSeoMeta({
+  title: 'Nuxt練習',
+  description: '練習Nuxt框架的使用，並且不使用路由來製作一個簡易網站。',
+  ogTitle: 'Nuxt練習',
+  ogDescription: '練習Nuxt框架的使用，並且不使用路由來製作一個簡易網站。',
+  keywords:'練習,學習,Nuxt3,vue3',
+})
 </script>
+
+<style scoped lang="scss">
+.showView{
+  padding: 16px;
+}
+</style>
