a1bf4f32c9ae20b34cd5b17e24983622f5159aae
xcia4899 <xcia48996531@gmail.com>
2026-01-08 14:12:18 +0800

    updata

--- app.vue
+++ app.vue
@@ -7,6 +7,21 @@
 </template>
 
 <script setup lang="ts">
+const config = useRuntimeConfig();
+
+const isWaking = ref(true);
+
+onMounted(async () => {
+  try {
+    // 1️⃣ 喚醒 Render（免費方案）
+    await fetch(`${config.public.apiBaseURL}/health`);
+  } catch (err) {
+    console.warn("Backend wake up failed", err);
+  } finally {
+    isWaking.value = false;
+  }
+});
+
 useSeoMeta({
   title: 'Nuxt練習',
   description: '練習Nuxt框架的使用，並且不使用路由來製作一個簡易網站。',
@@ -14,6 +29,7 @@ useSeoMeta({
   ogDescription: '練習Nuxt框架的使用，並且不使用路由來製作一個簡易網站。',
   keywords:'練習,學習,Nuxt3,vue3',
 })
+
 </script>
 
 <style scoped lang="scss">
